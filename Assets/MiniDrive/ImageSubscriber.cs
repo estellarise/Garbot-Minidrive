02fc1ec8299fee7c7453370abf678d75b5285b13
David Goedicke <tsaimelina@gmail.com>
2023-06-27 11:03:16 -0400

    Display image size is correct on Unity: both dimensions in game and actual pictures showing up (instead of square of changing colors). ESP Cam freezes after a few seconds.

--- Assets/MiniDrive/ImageSubscriber.cs
+++ Assets/MiniDrive/ImageSubscriber.cs
@@ -13,19 +13,23 @@ public class ImageSubscriber : MonoBehaviour
 {
     public Texture2D VideoTexture2D; 
     public Image imageVideo;
+    private int espCamWidth = 320;
+    private int espCamHeight = 240;
     void Start()
     {
-        VideoTexture2D = new Texture2D(2, 2);
-        ROSConnection.GetOrCreateInstance().Subscribe<CompressedImageMsg>("esp32_img/compressed", ImageChange); 
-            // moving to update freezes connection, subscriber inherently a loop, no need to loop it
+        VideoTexture2D = new Texture2D(espCamWidth, espCamHeight);
+        ROSConnection.GetOrCreateInstance().Subscribe<CompressedImageMsg>("esp32_img/compressed", ImageChange);
+        // moving to update freezes connection, subscriber inherently a loop, no need to loop it
     }
 
     void ImageChange(CompressedImageMsg imageMessage)
     {
+        //print(imageMessage.header);
+        //print(imageMessage.format);
         //imageVideo.image = ImageConversion.LoadImage(imageVideo, imageMessage.data);
         // ImageConversion.LoadImage(VideoTexture2D, imageMessage.data);
         VideoTexture2D.LoadImage(imageMessage.data);
-        Sprite newImage = Sprite.Create(VideoTexture2D, new Rect(2,2,2, 2), Vector2.zero);
+        Sprite newImage = Sprite.Create(VideoTexture2D, new Rect(0,0,espCamWidth, espCamHeight), Vector2.zero);
         imageVideo.sprite = newImage;
         //GetComponent<Renderer>().material.mainTexture = VideoTexture2D;
         //cube.GetComponent<Renderer>().material.color = new Color32((byte)colorMessage.r, (byte)colorMessage.g, (byte)colorMessage.b, (byte)colorMessage.a);
